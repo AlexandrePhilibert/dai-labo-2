98ab38cb03a1c8a9ad1bb738f143a2ff004dec20
Valentin Ricard <91870700+valentin-ricard@users.noreply.github.com>
2023-11-18 11:28:48 +0100

    feat(parsing): Implemented rudimentary command parsing
    Not distinction is currently made between server and client commands.

--- src/main/java/ch/heigvd/exceptions/ParseException.java
+++ src/main/java/ch/heigvd/exceptions/ParseException.java
@@ -1,4 +1,10 @@
 package ch.heigvd.exceptions;
 
 public class ParseException extends Exception {
+    public ParseException() {
+
+    }
+    public ParseException(String contents) {
+        super(contents);
+    }
 }
---
+++ src/main/java/ch/heigvd/parser/CommandParser.java
@@ -0,0 +1,23 @@
+package ch.heigvd.parser;
+
+import ch.heigvd.commands.*;
+import ch.heigvd.exceptions.ParseException;
+
+public class CommandParser {
+    public static Command parse(String line) throws ParseException {
+        // Get the start
+        String command = line.split(" ")[0];
+        return switch (command) {
+            case "CONNECT" -> Connect.from(line);
+            case "ACCEPT" -> Accept.from(line);
+            case "DECLINE" -> Decline.from(line);
+            case "SEND" -> Send.from(line);
+            case "ACK" -> Acknowledge.from(line);
+            case "SYNC" -> Sync.from(line);
+            case "MSG" -> Message.from(line);
+            case "ENDSYNC" -> EndSync.from(line);
+
+            default -> throw new ParseException("Unknown command!");
+        };
+    }
+}
